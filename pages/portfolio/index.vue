b702d08e25ec96841a3370626165bf7204c85084
luszczeksebastian@gmail.com <luszczeksebastian@gmail.com>
2019-06-28 10:02:05 +0200

    styles

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -2,12 +2,11 @@
   <div class="container portfolio">
     <div>
       <h1 class="title">Portfolio</h1>
-      <h2 class="subtitle">My nuxt.js portfolio project with simple markdown blog</h2>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa, minima accusamus. Nostrum optio assumenda eos
-        magni alias placeat ad error modi autem labore. Repellat error provident expedita nam aliquam ea rerum, iure
-        officia natus ab, repudiandae rem consequatur placeat animi iusto corrupti accusamus vitae quo nobis. Expedita,
-        necessitatibus similique quia atque inventore doloremque facere, eaque incidunt labore, at suscipit quae
-        fugit?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae corporis deleniti dicta dolor ducimus eius
+        eos error, eum ex excepturi expedita fugiat in ipsa ipsam labore laudantium magni molestias nemo neque nesciunt
+        nihil obcaecati placeat possimus quibusdam quisquam quod quos rem tempore tenetur ullam ut, veritatis voluptate!
+        Aspernatur assumenda commodi culpa delectus dignissimos eligendi, est id ipsa ipsum laborum non nostrum placeat
+        qui quod recusandae suscipit vel. Ab aliquam, aliquid.</p>
 
       <div v-for="(post,key) in projectList" :key="key" class="project-post">
         <nuxt-link :to="`/portfolio/${post.slug}`" class="post-content">
@@ -18,7 +17,8 @@
             <h2 class="title">{{ post.title }}</h2>
             <p class="subtitle">{{ post.description }}</p>
             <div class="tech_icons">
-              <img v-for="icon in (post.tech.split(','))" :key="icon" :src="`/icons/${icon}.png`" :alt="icon" class="tech_icon">
+              <img v-for="icon in (post.tech.split(','))" :key="icon" :src="`/icons/${icon}.png`" :alt="icon"
+                   class="tech_icon">
             </div>
           </div>
         </nuxt-link>
@@ -54,6 +54,7 @@
       border: 1px solid #eee;
       box-shadow: 3px 3px 7px #eee;
       transition: .3s;
+
       &:hover {
         transform: translate(-3px, -3px);
         box-shadow: 5px 5px 10px #eee;
@@ -77,15 +78,18 @@
           width: 100%;
         }
       }
+
       .description {
         position: relative;
         height: 100%;
+
         .tech_icons {
           width: 100%;
           text-align: right;
           position: absolute;
           bottom: 10px;
           right: 10px;
+
           .tech_icon {
             display: inline-block;
             height: 48px;
