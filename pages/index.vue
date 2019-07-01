c0ac922f9a4b57dd2762201a3114f08ed657651f
luszczeksebastian@gmail.com <luszczeksebastian@gmail.com>
2019-07-01 15:59:48 +0200

    nuxtServerInit check 2

--- pages/index.vue
+++ pages/index.vue
@@ -3,20 +3,19 @@
     <div>
       <h1 class="title">Home</h1>
       <h2 class="subtitle">My nuxt.js portfolio project with simple markdown blog</h2>
-      <p>{{blogs}}</p></div>
+     </div>
   </div>
 </template>
 
 <script>
   export default {
-    data() {
-      return {
-        blogs: null
-      }
-    },
     fetch({store, redirect}) {
-      this.blogs = store.state.bloglist();
       redirect(301, '/portfolio')
+    },
+    computed: {
+      projectList() {
+        return this.$store.state.projectlist;
+      }
     }
   }
 </script>
